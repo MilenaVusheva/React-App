ef68db427e076e2038c508fd298f74d11164b863
MilenaVusheva <88533151+MilenaVusheva@users.noreply.github.com>
2023-05-19 14:21:51 +0300

    Update likeService.js

--- spa/src/services/likeService.js
+++ spa/src/services/likeService.js
@@ -5,10 +5,6 @@ const baseUrl = 'http://localhost:3030/api';
 export const like = (userId, petId) => request.post(`${baseUrl}/pets/like/${petId}/${userId}`, {userId, petId});
 
 export const getPetLikes = (petId , userId) => {
-    //const query = encodeURIComponent(`petId="${petId}"`);
-
-    //return request.get(`${baseUrl}/likes?select=userId&where=${query}`)
-    
     return request.get(`${baseUrl}/pets/details/likes/${petId}`)
         .then(res => res.map(x => x.userId));
 }
